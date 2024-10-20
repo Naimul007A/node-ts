d014c87d4832f23d080e6ef47f35fe07575b7913
Naimul islam <mdnaimul2090@gmail.com>
2024-10-20 13:07:16 +0600

    feat:Add Zod and remove joi dependence

--- src/app/utils/error.ts
+++ src/app/utils/error.ts
@@ -1,25 +1,31 @@
 import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
+import { ZodError } from "zod";
 
 //Throw error properly
-async function errorHandler(
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+async function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
   //@ts-ignore
   if (err.isJoi === true) {
     //@ts-ignore
     err.status = 422;
   }
+    let messages: string | object = err.message;
+    if (err instanceof ZodError) {
+        const formattedErrors = err.issues.reduce((errors: { [key: string]: string }, issue) => {
+            errors[issue.path[0] as string] = issue.message;
+            return errors;
+        }, {});
+        //@ts-ignore
+        err.status = 422;
+        messages = formattedErrors;
+    }
   //@ts-ignore
   err.status = err.status || 500;
   res.send({
     error: {
       //@ts-ignore
       status: err.status || 500,
-      message: err.message,
+          message: messages,
     },
   });
 }
